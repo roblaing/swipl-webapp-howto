19bdb8202ba66f7e5fae4b8d1b4ceee143d14bd1
roblaing <robert.laing@rocketmail.com>
2019-05-15 11:26:29 +0200

    ASCII-chan working, now to Unit 4

--- unit3/server.pl
+++ unit3/server.pl
@@ -20,7 +20,13 @@ db_insert(Title, Art) :-
   sql_escape_single_quotes(Title, ETitle),
   sql_escape_single_quotes(Art, EArt),  
   odbc_connect('blog', Connection, []),
-  odbc_query(Connection, "INSERT INTO arts (title, art) VALUES (\'~w\', \'~w\')"-[ETitle, EArt]),
+  odbc_query(Connection, "INSERT INTO arts (title, art) VALUES ('~w', '~w')"-[ETitle, EArt]),
+  odbc_disconnect(Connection).
+
+db_select(ArtList) :-
+  odbc_connect('blog', Connection, []),
+  odbc_query(Connection, "SELECT title, art FROM arts ORDER BY created DESC", 
+    ArtList, [findall(row(Title, Art), row(Title, Art))]),  
   odbc_disconnect(Connection).
 
 front_handler(Request) :-
@@ -30,31 +36,39 @@ front_handler(Request) :-
 front_handler(Request) :-
   member(method(post), Request), !,
   http_parameters(Request, [title(Title, [default('')]), art(Art, [default('')])]),
-  ((string_length(Title, 0) ; string_length(Art, 0)) -> Error = 'You need a title and art' 
-                                                      ; Error = '', db_insert(Title, Art)),
-  render_page(Title, Art, Error).
+  ((string_length(Title, 0) ; string_length(Art, 0)) -> render_page(Title, Art, 'You need a title and art') 
+                                                      ; db_insert(Title, Art), render_page('','','')).
 
-render_page(Title1, Art1, Error) :-
-  phrase(html({|html(Title1, Art1, Error)||
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <title>Unit 3</title>
-      <link rel="stylesheet" href="/styles/basic.css">
-    </head>
-    <body>
-      <form method="POST">
-        <label for="title">Title:</label>
-        <input type="text" name="title" value="Title1">
-        <label for="art">Art:</label>
-        <textarea name="art">Art1</textarea>
-        <div class="error">Error</div>
+render_page(Title, Art, Error) :-
+  arts_html(ArtsHtml),
+  reply_html_page([\['
+    <title>Unit 3</title>
+    <link rel="stylesheet" href="/styles/basic.css">
+    ']],
+    [\['<form method="POST">
+        <label for="title">Title</label>
+        <input type="text" name="title" value="'], 
+     Title, 
+     \['">
+        <label for="art">Art</label>
+        <textarea name="art">'], 
+     Art, 
+     \['</textarea>
+        <div class="error">'], 
+     Error, 
+     \['</div>
         <button type="submit">Send your art</button>
-      </form>
-    </body>
-    </html>
-    |}), TokenizedHtml),
-  format('Content-type: text/html~n~n'),
-  print_html(TokenizedHtml).
+        </form>
+        <hr>'],
+     \[ArtsHtml]]).
+  
+art_html(row(Title, Art), HtmlString) :-
+  atomics_to_string(['<div class="post">\n<div class="post-title">\n', 
+    Title, '\n</div>\n<pre class="post-content">\n', Art, '\n</pre>\n</div>\n'], '', HtmlString).
+
+arts_html(Html) :-
+  db_select(ArtList),
+  maplist(art_html, ArtList, HtmlList),
+  atomics_to_string(HtmlList, '', Html).
+  
 
